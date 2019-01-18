a0b49e5fd292ceb862fd9dd6d320e75f43973d89
rclaver <rclaver@xtec.cat>
2019-01-18 12:48:13 +0100

    primera función upgrader_1 para actualizar versión

---
+++ projects/documentation/upgrader/upgrader_1.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * upgrader_1: Transforma los datos del proyecto "documentation"
+ *             desde la estructura de la versión 0 a la estructura de la versión 1
+ * @author rafael
+ */
+if (!defined("DOKU_INC")) die();
+
+class upgrader_1 {
+
+    public function process() {
+
+    }
+}
