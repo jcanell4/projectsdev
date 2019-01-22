9470851e832ae14b17475b6e0d5a25ad8d0ad046
rclaver <rclaver@xtec.cat>
2019-01-22 14:48:46 +0100

    corrección para FactoryAuthorization del platreballfp (por haberlo movido de directorio)

--- projects/platreballfp/authorization/FactoryAuthorization.php
+++ projects/platreballfp/authorization/FactoryAuthorization.php
@@ -4,15 +4,17 @@
  * @author Rafael Claver
  */
 if (!defined('DOKU_INC')) die();
-if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN', DOKU_INC."lib/plugins/");
-require_once(DOKU_PLUGIN."wikiiocmodel/authorization/ProjectFactoryAuthorization.php");
+if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN', DOKU_INC . "lib/plugins/");
+if (!defined('WIKI_IOC_MODEL')) define('WIKI_IOC_MODEL', DOKU_PLUGIN . "wikiiocmodel/");
+require_once(WIKI_IOC_MODEL . "authorization/ProjectFactoryAuthorization.php");
 
 class FactoryAuthorization extends ProjectFactoryAuthorization {
 
-    const PROJECT_AUTH = DOKU_PLUGIN."wikiiocmodel/projects/platreballfp/authorization/";
+    const PROJECT_AUTH = DOKU_PLUGIN . "projectsdev/projects/platreballfp/authorization/";
+    const DEFAULT_AUTH = WIKI_IOC_MODEL . "projects/defaultProject/authorization/";
 
-    public function __construct() {
-        parent::__construct(self::PROJECT_AUTH);
+    public function __construct($projectType=NULL) {
+        parent::__construct( ($projectType!==NULL) ? self::PROJECT_AUTH : self::DEFAULT_AUTH);
     }
 
     public function setAuthorizationCfg() {
