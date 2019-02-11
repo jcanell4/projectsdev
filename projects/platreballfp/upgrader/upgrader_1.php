7c878c47340db0914632ab6f54d1ad69aa2cbd5d
rclaver <rclaver@xtec.cat>
2019-02-11 10:39:27 +0100

    remodelación de las llamadas de prueba a funciones

--- projects/platreballfp/upgrader/upgrader_1.php
+++ projects/platreballfp/upgrader/upgrader_1.php
@@ -37,6 +37,18 @@ class upgrader_1 extends CommonUpgrader {
 
         /**************** PLANTILLAS **********************************************************/
         //LECTURA DE LOS FICHEROS ORIGINALES
+        $t0 = @file_get_contents("/home/rafael/clone/loe_con.v0");
+        $t1 = @file_get_contents("/home/rafael/clone/loe_con.txt");
+        $doc = @file_get_contents("/home/rafael/clone/loe_doc.txt");
+
+        //REPLACE
+        $aTokRep = [["\s+:title:Taula Unitats",
+                     "  :title:Apartats"],
+                    ["{#_DATE\(\"{##itemc\[inici\]##}\", \"\.\"\)_#}-{#_DATE\(\"{##itemc\[inici\]##}",
+                     "{#_DATE(\"{##itemc[inici]##}\", \".\")_#}-{#_DATE(\"{##itemc[final]##}"]];
+        $doc = $this->updateTemplateByReplace($doc, $aTokRep);
+
+        //LECTURA DE LOS FICHEROS ORIGINALES
         $t0 = @file_get_contents("/home/rafael/clone/continguts_00.txt");
         $t1 = @file_get_contents("/home/rafael/clone/continguts_01.txt");
         $doc = @file_get_contents("/home/rafael/clone/document_00.txt");
@@ -45,31 +57,34 @@ class upgrader_1 extends CommonUpgrader {
         $aTokDel = ["\s+\* té una ponderació .*\"Qualificació final QF\"\)\.",
                     "\<WIOCCL:IF.*\>",
                     "\s+\- Ha patit un accident.*a la convocatòria JT\."];
-        $t0 = $this->updateTemplateDelete($t0, $aTokDel);
-        $doc = $this->updateTemplateDelete($doc, $aTokDel);
+        $t0 = $this->updateTemplateByDelete($t0, $aTokDel);
+        $doc = $this->updateTemplateByDelete($doc, $aTokDel);
 
         //INSERT
         $aTokIns[] = ['regexp' => "^L'exercici .* formativa:$",
                       'text' => "\n  * té una ponderació en la **qualificació final** del mòdul (vegeu l'apartat \"Qualificació final QF\").",
                       'pos' => 1,
                       'modif' => "m"];
-        $t0 = $this->updateTemplateInsert($t0, $aTokIns);
-        $doc = $this->updateTemplateInsert($doc, $aTokIns);
+        $t0 = $this->updateTemplateByInsert($t0, $aTokIns);
+        $doc = $this->updateTemplateByInsert($doc, $aTokIns);
 
         //MOVE
         $aTokMov[] = ['regexp0' => "\s+\* té una durada màxima de.*\[##TODO_3:.*##\]\.$",
                       'regexp1' => "\s+\* té una ponderació .* \(vegeu l'apartat \"Qualificació final QF\"\)\.$",
                       'pos' => 1,
                       'modif' => "m"];
-        $t0 = $this->updateTemplateMove($t0, $aTokMov);
-        $doc = $this->updateTemplateMove($doc, $aTokMov);
+        $t0 = $this->updateTemplateByMove($t0, $aTokMov);
+        $doc = $this->updateTemplateByMove($doc, $aTokMov);
 
         /************************* Tratamiento de PLANTILLAS *************************************/
 
         // Aplica una nueva plantilla a un documento creado con una plantilla antigua
-        $t0 = @file_get_contents("/home/rafael/clone/continguts0.txt");
-        $t1 = @file_get_contents("/home/rafael/clone/continguts1.txt");
-        $doc = @file_get_contents("/home/rafael/clone/document.txt");
+//        $t0 = @file_get_contents("/home/rafael/clone/continguts0.txt");
+//        $t1 = @file_get_contents("/home/rafael/clone/continguts1.txt");
+//        $doc = @file_get_contents("/home/rafael/clone/document.txt");
+        $t0 = @file_get_contents("/home/rafael/clone/loe_con.v0");
+        $t1 = @file_get_contents("/home/rafael/clone/loe_con.txt");
+        $doc = @file_get_contents("/home/rafael/clone/loe_doc.txt");
         $dataChanged = $this->updateDocToNewTemplate($t0, $t1, $doc);
 
         // Aplica una nueva plantilla, con token numerado, a un documento creado con una plantilla antigua
@@ -81,7 +96,7 @@ class upgrader_1 extends CommonUpgrader {
         // Elimina de un documento los trozos de texto que cumplen con cada una de las expresiones regulares
         $doc = @file_get_contents("/home/rafael/clone/document_00.txt");
         $aTokDel = ["<WIOCCL:IF.*>","[##TODO_6:.*##]."];
-        $dataChanged = $this->updateTemplateDelete($doc, $aTokDel);
+        $dataChanged = $this->updateTemplateByDelete($doc, $aTokDel);
         //$this->model->setDataProject(json_encode($dataChanged), "Upgrade: version 0 to 1");
 
         // Inserta en el documento trozos de texto en las posiciones indicadas por cada una de las expresiones regulares
@@ -89,7 +104,7 @@ class upgrader_1 extends CommonUpgrader {
         $aTokIns[] = ['regexp' => "^L'exercici .* formativa:$",
                       'text' => "\n  * té una ponderació en la **qualificació final** del mòdul (vegeu l'apartat \"Qualificació final QF\".",
                       'pos' => 1];
-        $dataChanged = $this->updateTemplateInsert($doc, $aTokIns);
+        $dataChanged = $this->updateTemplateByInsert($doc, $aTokIns);
         //$this->model->setDataProject(json_encode($dataChanged), "Upgrade: version 0 to 1");
 
         // Aplica una nueva plantilla a un documento creado con una plantilla antigua
@@ -98,7 +113,7 @@ class upgrader_1 extends CommonUpgrader {
         $doc = @file_get_contents("/home/rafael/clone/document_00.txt");
         $token0 = "\[##TODO.*##\]";
         $aTokens = ["0","1","2","3"];
-        $dataChanged = $this->updateTemplateReplace($t0, $t1, $doc, $token0, $aTokens);
+        $dataChanged = $this->updateTemplateReplacingTokens($t0, $t1, $doc, $token0, $aTokens);
         //$this->model->setDataProject(json_encode($dataChanged), "Upgrade: version 0 to 1");
     }
 
