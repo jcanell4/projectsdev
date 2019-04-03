4e4f2519fec82af7480de4766e0a278d1b0a4ed5
rclaver <rclaver@xtec.cat>
2019-04-03 10:54:53 +0200

    corregir para múltiples ficheros de proyecto

--- projects/guiesges/actions/SetProjectMetaDataAction.php
+++ projects/guiesges/actions/SetProjectMetaDataAction.php
@@ -3,12 +3,14 @@ if (!defined('DOKU_INC')) die();
 
 class SetProjectMetaDataAction extends BasicSetProjectMetaDataAction {
 
-     protected function responseProcess(){
-         $response = parent::responseProcess();
-         if($this->getModel()->isProjectGenerated()){
-             $id = $this->getModel()->getContentDocumentId($response);
-             p_set_metadata($id, array('metadataProjectChanged' => time()));
-         }
-         return $response;
-     }
+    protected function responseProcess(){
+        $response = parent::responseProcess();
+        if ($this->getModel()->isProjectGenerated()) {
+            $llista = $this->getModel()->llistaDePlantilles();
+            foreach ($llista as $p) {
+                p_set_metadata($p, array('metadataProjectChanged' => time()));
+            }
+        }
+        return $response;
+    }
 }
