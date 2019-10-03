fb133c07bf605a768869b9bc9a64360aa7ef9996
rclaver <rclaver@xtec.cat>
2019-10-03 14:47:40 +0200

    modifyACLPageAndShortcutToPerson

--- projects/documentation/datamodel/documentationProjectModel.php
+++ projects/documentation/datamodel/documentationProjectModel.php
@@ -6,8 +6,6 @@
 if (!defined("DOKU_INC")) die();
 if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN', DOKU_INC . 'lib/plugins/');
 if (!defined('WIKI_IOC_MODEL')) define('WIKI_IOC_MODEL', DOKU_PLUGIN . 'wikiiocmodel/');
-
-require_once (WIKI_IOC_MODEL . "authorization/PagePermissionManager.php");
 require_once (WIKI_IOC_MODEL . "datamodel/AbstractProjectModel.php");
 
 class documentationProjectModel extends AbstractProjectModel {
@@ -25,36 +23,14 @@ class documentationProjectModel extends AbstractProjectModel {
         //1. Crea el archivo 'continguts', en la carpeta del proyecto, a partir de la plantilla especificada
         $this->createPageFromTemplate($destino, $plantilla, NULL, "generate project");
 
-        $aAutors = preg_split("/[\s,]+/", $ret['projectMetaData']["autor"]['value']);
-        foreach ($aAutors as $autor) {
-            //2a. Otorga, a cada Autor, permisos sobre el directorio de proyecto
-            PagePermissionManager::updatePagePermission($this->id.":*", $autor, AUTH_UPLOAD);
-
-            //3a. Otorga permisos a cada Autor sobre su propio directorio (en el caso de que no los tenga)
-            $ns = WikiGlobalConfig::getConf('userpage_ns','wikiiocmodel').$autor.":";
-            PagePermissionManager::updatePagePermission($ns."*", $autor, AUTH_DELETE, TRUE);
-            //3b. Incluye la página del proyecto en el archivo de atajos del Autor
-            $params = [
-                 'id' => $this->id
-                ,'link_page' => $ret['projectMetaData']["fitxercontinguts"]['value']
-                ,'user_shortcut' => $ns.WikiGlobalConfig::getConf('shortcut_page_name','wikiiocmodel')
-            ];
-            $this->includePageProjectToUserShortcut($params);
-        }
-
-        //2b. Otorga, a los Responsables, permisos sobre el directorio de proyecto
-        $aResponsables = preg_split("/[\s,]+/", $ret['projectMetaData']["responsable"]['value']);
-        foreach ($aResponsables as $responsable) {
-            if (! in_array($responsable, $aAutors)) {
-                PagePermissionManager::updatePagePermission($this->id.":*", $responsable, AUTH_UPLOAD);
-            }
-        }
+        //3. Otorga, a cada 'person', permisos adecuados sobre el directorio de proyecto y añade shortcut
+        $params = $this->buildParamsToPersons($ret['projectMetaData'], NULL);
+        $this->modifyACLPageAndShortcutToPerson($params);
 
         //4. Establece la marca de 'proyecto generado'
         $ret[ProjectKeys::KEY_GENERATED] = $this->projectMetaDataQuery->setProjectGenerated();
 
         return $ret;
     }
-
 
 }
