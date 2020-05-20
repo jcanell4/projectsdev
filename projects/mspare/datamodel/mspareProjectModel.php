89e6799c34666227f282a46d3393371a867cd26b
Josep <jcanell4@xtec.cat>
2020-05-20 14:30:51 +0200

    Es puja el mètode renameProject al pare

--- projects/mspare/datamodel/mspareProjectModel.php
+++ projects/mspare/datamodel/mspareProjectModel.php
@@ -20,30 +20,6 @@ class mspareProjectModel extends AbstractProjectModel {
     }
 
     /**
-     * Canvia el nom dels directoris del projecte indicat,
-     * els noms dels fitxers generats amb la base del nom del projecte i
-     * les referències a l'antic nom de projecte dins dels fitxers afectats
-     * @param string $ns : ns original del projecte
-     * @param string $new_name : nou nom pel projecte
-     * @param string $persons : noms dels autors i els responsables separats per ","
-     */
-    public function renameProject($ns, $new_name, $persons) {
-        $base_dir = explode(":", $ns);
-        $old_name = array_pop($base_dir);
-        $base_dir = implode("/", $base_dir);
-
-        $this->projectMetaDataQuery->renameDirNames($base_dir, $old_name, $new_name);
-        $this->projectMetaDataQuery->changeOldPathInRevisionFiles($base_dir, $old_name, $new_name);
-        $this->projectMetaDataQuery->changeOldPathInACLFile($old_name, $new_name);
-        $this->projectMetaDataQuery->changeOldPathProjectInShortcutFiles($old_name, $new_name, $persons);
-        $this->projectMetaDataQuery->renameRenderGeneratedFiles($base_dir, $old_name, $new_name, $this->listGeneratedFilesByRender($base_dir, $old_name));
-        $this->projectMetaDataQuery->changeOldPathInContentFiles($base_dir, $old_name, $new_name);
-
-        $new_ns = preg_replace("/:[^:]*$/", ":$new_name", $ns);
-        $this->setProjectId($new_ns);
-    }
-
-    /**
      * Devuelve la lista de archivos que se generan a partir de la configuración
      * indicada en el archivo 'configRender.json'
      * Esos archivos se guardan en WikiGlobalConfig::getConf('mediadir')
