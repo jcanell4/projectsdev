f7d40025748618d37f10a8858a2665ab997b9d7f
rclaver <rclaver@xtec.cat>
2021-01-31 09:48:44 +0100

    public function array_in_array

--- projects/prgfploe/actions/ImportProjectAction.php
+++ projects/prgfploe/actions/ImportProjectAction.php
@@ -22,12 +22,29 @@ class ImportProjectAction extends ProjectMetadataAction {
             // 2. Verificar permisos sobre el proyecto a importar
             //
             $permissions = $jsonConfig['actions'][$actionCommand]['permissions'];
-            $modelManager = AbstractModelManager::Instance($importProjectType);
-            $buttonAuthorization = $modelManager->getAuthorizationManager('editProject');
+            $external_modelManager = AbstractModelManager::Instance($importProjectType);
+            $external_authorization = $external_modelManager->getAuthorizationManager('editProject');
+            $has_perm_group = $this->array_in_array($permissions['groups'], $external_authorization->getAllowedGroups());
+            $has_perm_rol = $this->array_in_array($permissions['rols'], $external_authorization->getAllowedRoles());
+            if ($has_perm_group || $has_perm_rol) {
+                throw new Exception("Petición correcta: $importProjectType");
+            }
         }else {
             throw new Exception("El tipus de projecte $importProjectType no és un tipus vàlid per a la importació.");
         }
         throw new Exception("Action no definida. Petición: $importProjectType");
+    }
+
+    // Busca si algún elemento de $array1 está incluido en $array2
+    public function array_in_array($array1, $array2) {
+        $has = FALSE;
+        foreach ($array1 as $elem) {
+            if (in_array($elem, $array2)) {
+                $has = TRUE;
+                break;
+            }
+        }
+        return $has;
     }
 
 }
