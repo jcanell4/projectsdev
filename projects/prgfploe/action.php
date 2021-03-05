2c7df07b278e82a020d303bb621081b32f7f3533
rclaver <rclaver@xtec.cat>
2021-03-05 16:49:01 +0100

    nou prgfploe_projectExport

--- projects/prgfploe/action.php
+++ projects/prgfploe/action.php
@@ -16,6 +16,7 @@ class action_plugin_projectsdev_projects_prgfploe extends WikiIocProjectWorkflow
         $controller->register_hook('ADD_TPL_CONTROLS', "AFTER", $this, "addWikiIocButtons", array());
         $controller->register_hook('ADD_TPL_CONTROL_SCRIPTS', "AFTER", $this, "addControlScripts", array());
         $controller->register_hook('WIOC_PROCESS_RESPONSE_project', "AFTER", $this, "setExtraMeta", array());
+        $controller->register_hook('WIOC_PROCESS_RESPONSE_projectExport', "AFTER", $this, "setExtraMeta", array());
     }
 
     /**
