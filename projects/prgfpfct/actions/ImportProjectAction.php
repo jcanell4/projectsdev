ad1c6cd8655b1a8fed8060bc65ead8101a0ca7be
rclaver <rclaver@xtec.cat>
2021-05-20 14:26:04 +0200

    correcció tipus de dades a importar

--- projects/prgfpfct/actions/ImportProjectAction.php
+++ projects/prgfpfct/actions/ImportProjectAction.php
@@ -37,34 +37,28 @@ class ImportProjectAction extends ViewProjectAction {
 
                 // 3. Verificar una importació anterior
                 if (empty($import_data['nsProgramacio'])) {
-                    
-                    // 4. Verificar que les dades a importar son del tipus adequat
-                    if (!$import_data["tipusCicle"] || $import_data["tipusCicle"] == "LOE"){
-                        // Taula d'importació
-                        //camps directes
-                        $dataProject['tipusCicle'] = $import_data['tipusCicle'];
-                        $dataProject['cicle']      = $import_data['cicle'];
-                        $dataProject['durada']     = $import_data['durada'];
-                        
-                        $summary = "Importació de dades correcta des del projecte '{$this->params['project_import']}'";
-                        if ($model->setDataProject(json_encode($dataProject), $summary)) {
-                            $resp['info'] = self::generateInfo("info", $summary, $projectID);
-                        }else {
-                            $resp['info'] = self::generateInfo("error", "Error en la $summary", $projectID);
-                            $resp['alert'] = "Error en la $summary";
-                        }
+                    // 4. importar dades
+                    //camps directes
+                    $dataProject['tipusCicle'] = $import_data['tipusCicle'];
+                    $dataProject['cicle']      = $import_data['cicle'];
+                    $dataProject['durada']     = $import_data['durada'];
 
-                        $import_data['nsProgramacio'] = $projectID;
-                        if (! $import_metaDataQuery->setMeta(json_encode($import_data), "main", "Dades importades pel projecte '$projectID'")) {
-                            $resp['info'] = self::generateInfo("error", "Error en l'actualització de les dades a '{$this->params['project_import']}' després de la importació.", $projectID);
-                        }
-                        
-                        $response = parent::responseProcess();
-                        if ($resp["info"]) $response["info"] = $resp["info"];
-                        if ($resp["alert"]) $response["alert"] = $resp["alert"];
+                    $summary = "Importació de dades correcta des del projecte '{$this->params['project_import']}'";
+                    if ($model->setDataProject(json_encode($dataProject), $summary)) {
+                        $resp['info'] = self::generateInfo("info", $summary, $projectID);
                     }else {
-                        throw new Exception("El tipus de projecte no és de tipus LOE i no és vàlid per a la importació.");
+                        $resp['info'] = self::generateInfo("error", "Error en la $summary", $projectID);
+                        $resp['alert'] = "Error en la $summary";
                     }
+
+                    $import_data['nsProgramacio'] = $projectID;
+                    if (! $import_metaDataQuery->setMeta(json_encode($import_data), "main", "Dades importades pel projecte '$projectID'")) {
+                        $resp['info'] = self::generateInfo("error", "Error en l'actualització de les dades a '{$this->params['project_import']}' després de la importació.", $projectID);
+                    }
+
+                    $response = parent::responseProcess();
+                    if ($resp["info"]) $response["info"] = $resp["info"];
+                    if ($resp["alert"]) $response["alert"] = $resp["alert"];
                 }else{
                     throw new Exception("El projecte '{$this->params['project_import']}' ja ha estat importat anteriorment.");
                 }
