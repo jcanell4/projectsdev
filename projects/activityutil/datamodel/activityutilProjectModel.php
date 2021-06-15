1999a0fd25a5ebd15e37d2f07c2471cb581543b7
rclaver <rclaver@xtec.cat>
2021-06-15 12:50:07 +0200

    Validació de la llista de documents dels projectes activityutil

--- projects/activityutil/datamodel/activityutilProjectModel.php
+++ projects/activityutil/datamodel/activityutilProjectModel.php
@@ -7,18 +7,60 @@ if (!defined("DOKU_INC")) die();
 
 class activityutilProjectModel extends MultiContentFilesProjectModel {
 
-    public function __construct($persistenceEngine)  {
+    public function __construct($persistenceEngine) {
         parent::__construct($persistenceEngine);
-        $this->needGenerateAction=false;
+        $this->needGenerateAction = false;
     }
 
-    public function generateProject() {} //abstract obligatorio
+    public function generateProject(){}
 
     public function directGenerateProject() {
-        //4. Establece la marca de 'proyecto generado'
         return $this->projectMetaDataQuery->setProjectGenerated();
     }
-    
+
+    /**
+     * Gestiona la llista de documents definits per l'usuari
+     * @param array $data : dades del projecte (camps del formulari actiu)
+     */
+    public function validateFields($data=NULL){
+        if ($data) {
+            $nousDocuments = json_decode($data['documents'], true);
+            
+            $dataProject = $this->getCurrentDataProject();
+            $vellsDocuments = json_decode($dataProject['documents'], true);
+
+            $nom_continguts = $this->_getContingutsName();
+            $template = $this->getRawProjectTemplate(substr($nom_continguts, 0, -4));
+            $path_continguts = WikiGlobalConfig::getConf('datadir')."/".str_replace(":", "/", $this->getId());
+
+            foreach ($nousDocuments as $k => $doc) {
+                if (!isset($vellsDocuments[$k]['nom'])) {
+                    file_put_contents("$path_continguts/{$doc['nom']}.txt", $template);
+                }
+                else if ($doc['nom'] !== $vellsDocuments[$k]['nom']) {
+                    rename("$path_continguts/{$vellsDocuments[$k]['nom']}.txt", "$path_continguts/{$doc['nom']}.txt");
+                }
+            }
+            //En el cas que s'hagin eliminat files de document
+            for ($i=$k+1; $i<count($vellsDocuments); $i++) {
+                unlink("$path_continguts/{$vellsDocuments[$i]['nom']}.txt");
+            }
+        }
+        else {
+            throw new Exception("Aquí passa alguna cosa rara");
+        }
+    }
+
+    private function _getContingutsName() {
+        $project_dir = $this->getProjectTypeDir()."/metadata/plantilles";
+        $list = scandir($project_dir);
+        foreach ($list as $file) {
+            if (is_file("$project_dir/$file")) {
+                return $file;
+            }
+        }
+    }
+
     /**
      * Obtiene la lista de ficheros creados, por el autor, en el proyecto
      * @return array de ficheros
